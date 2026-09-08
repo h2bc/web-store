a086b7609b596ac6df02dafab426477dcccff832
Henrikas Antanas <henrikasgir@gmail.com>
2026-09-08 19:22:41 +0000

    added video to the gallery

--- front/app/(main)/gallery/page.tsx
+++ front/app/(main)/gallery/page.tsx
@@ -4,6 +4,7 @@ export const metadata = {
 }
 
 const VIDEOS: { id: string; title: string }[] = [
+  { id: 'srRVUe4_wW4', title: 'verkei?' },
   { id: 'C8Hkml0CRmo', title: 'meduza' },
   { id: 'qI8fDbBXW2s', title: '2DRIP' },
 ]
